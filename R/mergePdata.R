5cc59724f8e7b3f47579b44bb2303f68a35898eb
David Ornelles <ornelles@wakehealth.edu>
2017-10-15 08:48:30 -0400

    Small edits

--- R/mergePdata.R
+++ R/mergePdata.R
@@ -9,32 +9,37 @@
 mergePdata <- function(phenoData, imageData)
 {
 # check arguments and harmonize well names
-	stopifnot("well" %in% names(phenoData))
-	phenoData$well <- factor(well.info(phenoData$well)$well)
-	stopifnot(levels(imageData$well) %in% levels(phenoData$well))
-	stopifnot("moi" %in% names(phenoData))
 	stopifnot("well" %in% names(imageData))
+	stopifnot("well" %in% names(phenoData))
+	stopifnot("moi" %in% names(phenoData))
+	phenoData$well <- factor(well.info(phenoData$well)$well) # harmonize
+	stopifnot(levels(imageData$well) %in% levels(phenoData$well))
 
-# add row and column information to data
+# add row and column information to phenotype data
 	phenoData$column <- well.info(phenoData$well)$column
 	phenoData$row <- well.info(phenoData$well)$row
 
+# check value for unit and assign control wells
 	if (!"unit" %in% names(phenoData))
-		phenoData$unit <- "unknown"
+		phenoData$unit <- "unspecified"
 	type <- rep("standard", nrow(phenoData))
 	type[phenoData$moi == 0] <- "control"
 	phenoData$type <- type
 	
-	sel <- names(phenoData) %in% c("well","column","row", "unit", "type")
+# reorganize phenotype data
+	sel <- names(phenoData) %in% c("well", "column", "row", "unit", "type")
 	phenoData <- cbind(phenoData[sel], phenoData[!sel])
 
-# remove variables already present in phenoData
+# remove variables in imageData that are already present in phenoData
 	keep <- names(imageData)[!names(imageData) %in% names(phenoData)]
 	imageData <- imageData[c("well", keep)]
 	res <- merge(phenoData, imageData)
+
+# convert strings to factors
 	sel <- sapply(res, is.character)
 	res[sel] <- lapply(res[sel], as.factor)
-# exclude unused levels to allow pd to hold more than required
+
+# exclude unused levels (allows pd to hold information than required)
 	res <- droplevels(res)
 	rownames(res) <- NULL
 	return(res)
