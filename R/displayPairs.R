a8c1c3a7e7078242a5896f24dcf303bc5aaea07b
David Ornelles <ornelles@wakehealth.edu>
2017-11-06 18:40:19 -0500

    Improved nucMask

--- R/displayPairs.R
+++ R/displayPairs.R
@@ -3,28 +3,34 @@
 #
 # display fluorescent (or DAPI) files in directory with file named in 'f'
 #
+# Arguments
+# f			path to DAPI image in directory with paired images
+# dnaStain	if TRUE, add blue nuclei to image, else show 2nd as green
+# width, offset, sigma	paramaters for nucMask
+# col		overlay color for mask 
+# ...	parameters passed to display
+#
+# Return
+#	side effect of plotting paired images, returned invisibly
+#
 #########################################################################################
 
-displayPairs <- function(f, dna = FALSE, k.nuc=1.2, width=32, offset=0.05) {
+displayPairs <- function(f, dnaStain = FALSE, width = 32,
+	offset = 0.05, sigma = 2, col = "lightyellow", ...)
+{
 	if(require(EBImage) == FALSE)
 		stop("requires EBImage")
 	if (missing(f)) f <- file.choose()
 	path <- dirname(f)
 	ff <- list.files(path, pattern="tif$", full=TRUE, ignore.case = TRUE)
 	x <- readImage(ff[seq(1, length(ff), 2)])
-	xb <- normalize(x)
-	xb <- medianFilter(xb, 2)
-	xb <- gblur(xb, 2)
-	xt <- thresh(xb, w=width, h=width, offset=offset)
-	xt <- fillHull(xt)
-	xd <- distmap(xt)
-	xw <- watershed(xd)
-	y <- readImage(ff[seq(2,length(ff),2)])
-	if (dna)
-		img <- rgbImage(blue=normalize(x, separate=FALSE))
+	xw <- nucMask(x, width=width, offset=offset, sigma=sigma)
+	y <- readImage(ff[seq(2, length(ff), 2)])
+	if (dnaStain)
+		img <- rgbImage(blue = normalize(x, separate = FALSE))
 	else
-		img <- rgbImage(green=normalize(y, separate=FALSE))
-	img <- paintObjects(xw, img, col="yellow")
-	display(img, title=basename(path))
+		img <- rgbImage(green = normalize(y, separate = FALSE))
+	img <- paintObjects(xw, img, col = col)
+	display(img, title = basename(path), ...)
 	invisible(img)
 }
--- R/nucMask.R
+++ R/nucMask.R
@@ -4,16 +4,18 @@
 # generate and return nuclear mask from dapi image or dapi file, uses thresh2
 #
 #	dapi	dapi image or character string of dapi file
+#	gamma	values less than 1 increase sensitivity for dim signals
 #	width	largest nuclear width used as w and h parameters for .thresh2()
 #	offset	offset parameter for .thresh2(), default of 0.05, use 0.01 for low contrast
-#	sigma	radius for medianFilter and gblur, default value of 2 routine 
+#	sigma	radius for medianFilter and gblur, 2 for routine, 5 for finely detailed 
 #
 #########################################################################################
 
-nucMask <- function(dapi, width = 36, offset = 0.05, sigma = 2) {
+nucMask <- function(dapi, width = 36, offset = 0.05, gamma = 1, sigma = 2) {
 	if (is.character(dapi) && file.exists(dapi))
 		dapi <- readImage(dapi)
-	x <- normalize(dapi)
+	x <- dapi^gamma
+	x <- normalize(x)
 	x <- medianFilter(x, sigma)
 	x <- gblur(x, sigma)
 	x <- thresh2(x, w = width, offset = offset)
