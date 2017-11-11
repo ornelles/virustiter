e75880c6a3d011da8245b2d0e723b6f23993d2e8
David Ornelles <ornelles@wakehealth.edu>
2017-11-11 13:59:50 -0500

    Update to handle file or well

--- R/plotPlate.R
+++ R/plotPlate.R
@@ -2,38 +2,39 @@
 # plotPlate
 #
 # display entire plate with lattice graphics (expects well values as "a03", etc.
+# otherwise treats it as a 
 #
 #########################################################################################
 
 plotPlate <- function(df, cex=1/2, alpha=1/2, main, invert.y=TRUE, ... ) {
-	if (!"well" %in% names(df))
-		stop("requires variable 'well' in ", deparse(substitute(df)))
 	library(lattice)
 
-	n <- nlevels(df$well)
-	if (n > 96)							# 384-well plate
-		{rows <- 32; columns <- 24}
-	else if (n > 48)					# 96-well plate
-		{rows <- 8; columns <- 12}
-	else if (n > 24)					# 48-well plate
-		{rows <- 6; columns <- 8}
-	else if (n > 12)					# 24-well plate
-		{rows <- 4; columns <- 6}
-	else if (n > 6)						# 12-well plate
-		{rows <- 3; columns <- 4}
-	else if (n == 6)					# 6-well plate
-		{rows <- 2; columns <- 3}
-	else								# fewer than 6
-		{rows <- 1; columns <- n}
-
-	if (any(paste(letters[1:8],rep(2:4,each=8),sep="") %in% levels(df$well)))
-		ww <- sprintf("%s%d", rep(letters[1:rows], each=columns), 1:columns)
+	if ("well" %in% names(df)) {
+		byWell <- TRUE
+		n <- nlevels(df$well)
+		if (n > 96)	{rows <- 32; columns <- 24}		# 384-well plate
+		else if (n > 48) {rows <- 8; columns <- 12}	# 96-well plate
+		else if (n > 24) {rows <- 6; columns <- 8}	# 48-well plate
+		else if (n > 12) {rows <- 4; columns <- 6}	# 24-well plate
+		else if (n > 6) {rows <- 3; columns <- 4}	# 12-well plate
+		else if (n == 6) {rows <- 2; columns <- 3}	# 6-well plate
+		else {rows <- 1; columns <- n}				# fewer than 6
+		if (any(paste(letters[1:8],rep(2:4,each=8),sep="") %in% levels(df$well)))
+			ww <- sprintf("%s%d", rep(letters[1:rows], each=columns), 1:columns)
+		else
+			ww <- sprintf("%s%02d", rep(letters[1:rows], each=columns), 1:columns)
+		df$well <- factor(df$well, levels=ww)	# revise levels
+		skip <- ! levels(df$well) %in% unique(as.character(df$well))
+	}
+	else if ("file" %in% names(df)) {
+		byWell <- FALSE
+		n <- nlevels(df$file)
+		flevels <- abbreviate(levels(df$file), 18, method = "both")
+		df$file <- factor(df$file, levels = flevels) 
+		rows <- columns <- ceiling(sqrt(n))
+	}
 	else
-		ww <- sprintf("%s%02d", rep(letters[1:rows], each=columns), 1:columns)
-
-	df$well <- factor(df$well, levels=ww)	# revise levels
-
-	skip <- ! levels(df$well) %in% unique(as.character(df$well))
+		stop("requires variable 'well' or 'file' in ", deparse(substitute(df)))
 
 	if (missing(main) & !("dir" %in% names(df)))
 		main <- Sys.Date()
@@ -43,9 +44,16 @@ plotPlate <- function(df, cex=1/2, alpha=1/2, main, invert.y=TRUE, ... ) {
 	if (!"positive" %in% names(df)) df$positive <- FALSE
 	if (invert.y) df$ym <- -df$ym
 
-	obj <- xyplot(ym ~ xm | well, data=df, group=positive, cex=cex, alpha=alpha,
+	if (byWell == TRUE) {
+		obj <- xyplot(ym ~ xm | well, data=df, group=positive, cex=cex, alpha=alpha,
 			as.table=TRUE, aspect="iso", layout=c(columns,rows), skip=skip,
 			xlab = "", ylab = "", scales = list(draw=FALSE), main = main, ...)
+	}
+	else {
+		obj <- xyplot(ym ~ xm | file, data=df, group=positive, cex=cex, alpha=alpha,
+			as.table=TRUE, aspect="iso", layout=c(columns,rows),
+ 			xlab = "", ylab = "", scales = list(draw=FALSE), main = main, ...)
+	}
 	plot(obj)
 	invisible(obj)
 }
