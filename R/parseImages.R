14e7290581464b4d190b8b6da209ef99d8f8e0c7
David Ornelles <ornelles@wakehealth.edu>
2018-12-07 18:42:00 -0500

    split reading and analyzing

--- R/parseImages.R
+++ R/parseImages.R
@@ -1,23 +1,12 @@
-#' Extract Fluorescent Intensities from Paired Microscopic Images
+#' Parse Paired Microscopic Images
 #'
-#' Extract the mean fluorescence intensity of DNA and a second fluorescent
+#' Extract the mean fluorescence intensity of a DNA and second fluorescent
 #' target image for individual cells from paired images.
 #'
-#' @param source A character vector identifying a directory or directories
-#'   with either multilayer tiff image files \emph{or} subdirectories identified
-#'   by well with separate, paired images within each subdirectory for each
-#'   well \emph{or} a character vector of image files organized as described.
-#' @param type A character string identifying the type of image files to parse
-#'   ("tif", "tiff", "jpeg", "jpg" or "png".)
-#' @param which.images An integer of length 2 or 3. The first two numbers indicate
-#'   the position of the DNA image and the target image. The optional
-#'   third number specifies the total number of images for each field. A value of
-#'   c(1, 2) indicates DNA first and target second. A value of c(2, 1) indicates
-#'   target first and DNA image second. A value of c(1, 2, 4)
-#'   indicates DNA image, target image, and two additional (ignored) images
-#'   such as a second fluorescent color and phase contrast image for each field.
-#' @param pattern Optional \code{grep} pattern as a character string used
-#'   by \code{list.files()} to select image files.
+#' @param nuc A list of nuclear images. If \code{tgt} is \code{NULL}, \code{nuc}
+#'   is assumed to be a list of length 2 with both nuclear and target images.
+#' @param tgt A list of fluorescent target images if \code{nuc} is a list of nuclear
+#'   images. If \code{NULL}, \code{nuc} must be a list of both image types. 
 #' @param args.nucMask A list of arguments passed to \code{nucMask()}.
 #' @param args.trimMask A list of arguments passed to \code{trimMask()} such as
 #'   \code{cutoff} or \code{k}. If this value is \code{NA}, no trimming is performed.
@@ -32,10 +21,11 @@
 #'
 #' @details
 #'
-#' This is the core function to read and parse image data in a suite of tools
+#' This is the core function to parse image data in a suite of tools
 #' implemented with \code{\link{EBImage}} intended to determine
-#' viral titers from sets of fluorescent micrographs. Typically, the images are
-#' acquired in pairs where the first of each pair
+#' viral titers from sets of fluorescent micrographs. The first argument
+#' to this function can be the result of \code{\link{getImages}}. The images
+#' provided to this function are pairs where the first of each pair
 #' is a DNA image and the second a fluorescent image of the viral target.
 #' Because individual cells are identified by the nuclear stain, it
 #' \emph{may} be beneficial to collect overexposed DNA images.
@@ -46,9 +36,10 @@
 #' moi can be expressed as virions (VP) per cell \emph{or} infectious
 #' units (IU) per cell \emph{or} a unit of volume (ml, ul, nl) per cell.
 #' These details are added to output of this function with the
-#' \code{mergePdata()} function. The default order is for the nuclear
-#' (typically DAPI) image to precede the viral antigen image, but this
-#' sequence can be adjusted with the \code{which.images} argument.
+#' \code{mergePdata()} function. The expected order is for the nuclear
+#' (typically DAPI) image to precede the viral antigen image. This
+#' sequence can be adjusted with \code{which.images} argument in
+#' the function \code{\link{getImages}}.
 #'
 #' Images associated with each moi can be individual files in a
 #' single directory where each directory is named for the well such as
@@ -108,11 +99,13 @@
 #' @examples
 #' # Note that execution of these examples can be rather slow...
 #'   path.by.folder <- system.file("extdata", "by_folder", package = "virustiter")
-#'   df.by.folder <- parseImages(path.by.folder)
+#'   images.by.folder <- getImages(path.by.folder)
+#'   df.by.folder <- parseImages(images.by.folder)
 #'   head(df.by.folder)
 #'
 #'   path.by.stack <- system.file("extdata", "by_stack", package = "virustiter")
-#'   df.by.stack <- parseImages(path.by.stack)
+#'   images.by.stack <- getImages(path.by.stack)
+#'   df.by.stack <- parseImages(images.by.stack)
 #'   head(df.by.stack)
 #'
 #' # plots
@@ -125,98 +118,26 @@
 #'
 #' @export
 #'
-parseImages <- function(source, type = "tiff", which.images = c(1, 2, 2),
-	pattern = NULL, args.nucMask = NULL, args.trimMask = NULL,
+parseImages <- function(nuc, tgt = NULL, args.nucMask = NULL, args.trimMask = NULL,
 	cellMask.flag = FALSE, equalize = FALSE)
 {
 # requires EBImage, ensure appropriate values for parameters
 	if (!require(EBImage))
 		stop("The 'EBImage' package must be installed with biocLite")
 
-# are all files or directories found in 'source' argument legitimate?
-	if (!all(file.exists(source)))
-		stop("not all files named in ", deparse(substitute(source)), " exist")
-
-	if (all(file.info(source)$isdir))
-		ff <- list.images(path = source, type = type, pattern = pattern)
-	else if (all(!file.info(source)$isdir))
-		ff <- source
-	else
-		stop("unable to use files/source in ", deparse(substitute(source)))
-
 # check on arguments
-	if (length(which.images) == 2)
-			which.images <- c(which.images, max(which.images))
-	if (length(which.images) != 3)
-		stop("'which.images' must be an integer vector of length 2 or 3")
-	if (which.images[3] != max(which.images))
-		stop("the third value in 'which.images' must be the largest")
-
-# Evaluate and read image data 
-	message("Evaluating images...", appendLF = FALSE)
-
-# extract fields to determine if images are organized by well or stack
-	spl <- strsplit(ff, "/")
-	field1 <- sapply(spl, tail, 1)
-	field2 <- sapply(spl, function(x) head(tail(x, 2), 1))
-	sel <- grepl("[[:alpha:]][[:digit:]]+$", field2) # test for well pattern
-
-# assign variables to direct processing
-	if (all(sel)) {
-		imageType <- "byWell"
-		well <- field2
-		filename <- NULL
+	if (is.null(tgt)) {
+		if (!is.list(nuc) || length(nuc) != 2)
+			stop("'nuc' must be a list oflength two")
+		dnaImages <- nuc[[1]]
+		mfiImages <- nuc[[2]]
 	}
-	else if (!any(sel)) {
-		imageType <- "byStack"
-		well <- NULL
-		filename <- field1
+	else {
+		if (!is.list(nuc) || !is.list(tgt))
+			stop("'nuc' and 'tgt' must be a list")
+		dnaImages <- nuc
+		mfiImages <- tgt
 	}
-	else
-		stop("\nunable to use mixture of image files in ", source, '"')
-
-# split image paths into related groups (by well or by file)
-	if (imageType == "byWell")
-		ffsplit <- split(ff, well)
-	else if (imageType == "byStack")
-		ffsplit <- split(ff, filename)
-	else
-		stop("\nunexpected value for 'imageType'")
-
-# read all images as a list and coerce to grayscale with a warning
-	img <- lapply(ffsplit, function(f) suppressWarnings(readImage(f)))
-	if (any(sapply(img, colorMode) != 0)) {
-		warning("images have been converted to grayscale by uniform RGB averaging")
-		img <- lapply(img, channel, "gray")
-	}
-
-# check for expected numbers of images
-	n_dna <- which.images[1]
-	n_mfi <- which.images[2]
-	n_field <- which.images[3]
-
-# are total images in each group sensible?
-	n <- sapply(img, function(x) dim(x)[3])
-	bad <- which(n %% n_field != 0)
-	if (length(bad > 1))
-		stop("\nThe number of images in ", paste(names(img)[bad], collapse = ", "),
-			" are not multiples of ", n_field)
-	else if (length(bad == 1))
-		stop("\nThe number of images in ", names(img)[bad],
-			" is not a multiple of ", n_field)
-
-# extract dna images
-	dnaImages <- lapply(img, function(x, first = n_dna, by = n_field) {
-		N <- dim(x)[3]
-		if (N <= 1) stop("\nUNEXPECTED image dimension in dnaImages") 
-		x[,,seq(first, N, by), drop = FALSE]})
-
-# extract mfi images
-	mfiImages <- lapply(img, function(x, first = n_mfi, by = n_field) {
-		N <- dim(x)[3]
-		if (N <= 1) stop("\nUNEXPECTED image dimension in mfiImages") 
-		x[,,seq(first, N, by), drop = FALSE]})
-	message("done")	# evaluating images
 
 # option to smooth and equalize mfi images
 	if (equalize == TRUE) {
@@ -226,7 +147,7 @@ parseImages <- function(source, type = "tiff", which.images = c(1, 2, 2),
 	}
 
 # initialize variable to collect results, initialize progress bar
-	nff <- length(ffsplit)
+	nff <- length(dnaImages)
 	ret <- rep(list(NULL), nff)
 	showProgress <- ifelse(nff > 2, TRUE, FALSE)
 
@@ -238,7 +159,7 @@ parseImages <- function(source, type = "tiff", which.images = c(1, 2, 2),
 #
 # process each group of files in turn
 #
-	for (IDX in seq_along(ffsplit)) {
+	for (IDX in seq_len(nff)) {
 		if (showProgress)
 			setTxtProgressBar(pb, IDX)
 
