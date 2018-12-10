45ba207d02568618b4d9b25eda89072a6786caad
David Ornelles <ornelles@wakehealth.edu>
2018-12-10 15:24:14 -0500

    renamed from list.images

---
+++ R/listImages.R
@@ -0,0 +1,36 @@
+#' List Image Files
+#'
+#' List image files in the given path.
+#'
+#' @param path A character vector of path names.
+#' @param type The type of image as defined by the file extension (tiff, jpeg, jpg or png).
+#' @param pattern Argument passed to list.files (grep pattern), default is NULL.
+#' @param full.names Argument passed to list.files, default is TRUE.
+#' @param recursive Argument passed to list.files, default is TRUE.
+#' @param ignore.case Argument passed to list.files, default is TRUE.
+#'
+#' @return
+#'
+#' Character vector of selected image filenames.
+#'
+#' @examples
+#'   path.by.folder <- system.file("extdata", "by_folder", package = "virustiter")
+#'   listImages(path.by.folder, pattern = "002")
+#'
+#' @export
+#' 
+listImages <- function(path = ".", type = c("tiff", "jpeg", "jpg", "png"),
+	pattern = NULL, full.names = TRUE, recursive = TRUE, ignore.case = TRUE)
+{
+# assign file extension pattern from 'type'
+	ext <- switch(match.arg(type),
+		tiff = "\\.tif{1,2}$",
+		jpeg = "\\.jpe{0,1}g$",
+		jpg = "\\.jpe{0,1}g$",
+		png = "\\.png$")
+# collect file list matching pattern and then by type (extension)
+	ff <- list.files(path = path, pattern = pattern, full.names = full.names,
+		recursive = recursive, ignore.case = ignore.case)
+	ff <- ff[grep(ext, ff, ignore.case = TRUE)]
+	return(ff)
+}
