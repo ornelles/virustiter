0bb6c754e05a63635bcd1af20a656bb3321f2608
David Ornelles <ornelles@wakehealth.edu>
2018-12-11 00:04:02 -0500

    correct code

--- R/getAIC.R
+++ R/getAIC.R
@@ -1,33 +1,29 @@
-#########################################################################################
-#
-# utility to evaluate AIC for fitted model(s) based on cut value(s)
-#
-#########################################################################################
-#' Obtain AIC for 'Cut' Value
+
+#' Obtain AIC for Given Background Value
 #'
-#' Apply a given \code{cut} value to create a fitted model (or models) and
+#' Apply a given \code{bgnd} value to create a fitted model (or models) and
 #' return the Akaike's Criterion for the GLM fitted model. \code{getFit()}
-#' is called for each value of \code{cut}.
+#' is called for each value of \code{bgnd}.
 #'
 #' @param df Annotated \code{data.frame} or list of \code{data.frame}s with
 #'   imaging results.
-#' @param cut Numeric vector of cutoff values to test.
+#' @param bgnd Numeric vector of background values to test.
 #' @param by A character string indicating the organization of the results as
 #'   being a single result or by column or by rows.
 #'
 #' @return
 #'
-#' The AIC value associated with the fit for each value of cut or list of AIC
+#' The AIC value associated with the fit for each value of bgnd or list of AIC
 #' values if a list of data frames was provided.
 #'
 #' @export
 #'
-getAIC <- function(df, cut, by = c("sequential", "column", "row"))
+getAIC <- function(df, bgnd, by = c("sequential", "column", "row"))
 {
 	if (!is.data.frame(df))
 		stop("'", deparse(substitute(df)), "' must be a data frame processed by score()")	
 	by <- match.arg(by)
-	dfList <- lapply(cut, function(x) score(df, x))
+	dfList <- lapply(bgnd, function(x) score(df, x))
 	fmList <- lapply(dfList, function(x) getFit(x, by = by))
 	if (class(class(fmList)[1])[1] == "list")
 		ans <- lapply(fmList, function(x) sapply(x, AIC))
