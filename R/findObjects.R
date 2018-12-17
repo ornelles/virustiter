d9d192041a7e5f6a9476936987feac20d2b39fd0
David Ornelles <ornelles@wakehealth.edu>
2018-12-16 23:54:00 -0500

    refined code

--- R/findObjects.R
+++ R/findObjects.R
@@ -1,15 +1,15 @@
 #' Find Objects
 #' 
 #' Identify objects in data.frame produced by \code{parseImages()} 
-#' as defined by the expression \code{expr}. 
+#' as specified by the expression \code{expr}. 
 #' 
-#' @param expr An \code{expression} evaluated in \code{'data'} return
-#'   a logical vector identifying objects (rows).
+#' @param expr An \code{expression} evaluated in \code{'data'} that returns
+#'   a logical vector identifying objects (rows) in \code{'data'}.
 #' @param mask An optional \code{Image} object in \code{Grayscale} mode or
 #'   an array containing object masks. Object masks are sets of pixels with
 #'   the same unique integer value.
 #' @param data A \code{data.frame} produced by \code{parseImages()} in which
-#'   \code{'expr'} can be evaluated.
+#'   \code{'expr'} will be evaluated.
 #' @param invert A \code{logical} value to invert the selection determined
 #'   by \code{'expr'}.
 #' 
@@ -33,6 +33,13 @@
 #'
 findObjects <- function(expr, data, mask, invert = FALSE)
 {
+	if (missing(expr) || missing(data)) {
+		usage <- c("findObjects examples:",
+			'  findObjects(area > 200, df) # identify objects with area > 200',
+			'  findObjects(positive == TRUE, df, mask) # mark positive objects in mask')
+		cat(usage, sep = "\n")
+		return(invisible(NULL))
+	}
 # check 'data' argument
 	if (!is(data, "data.frame"))
 		stop("'", deparse(substitute(data)), "' must be a data.frame")
@@ -42,8 +49,8 @@ findObjects <- function(expr, data, mask, invert = FALSE)
 
 # evaluate expression and assign to original data.frame
 	sel <- eval(substitute(expr), data)
-	varname <- tail(make.unique(c(names(data), "var")), 1)
-	data[[varname]] <- sel
+	vname <- tail(make.unique(c(names(data), "var")), 1)
+	data[[vname]] <- sel
 
 # split 'var' according to 'well/file' and 'frame' in 'data'
 	if ("well" %in% names(data))
@@ -51,7 +58,7 @@ findObjects <- function(expr, data, mask, invert = FALSE)
 	else if ("file" %in% names(data))
 		group <- "file"
 	spl.1 <- split(data, data[[group]], drop = TRUE)
-	spl.2 <- lapply(spl.1, function(v) split(v[[varname]], v[["frame"]], drop = TRUE))
+	spl.2 <- lapply(spl.1, function(v) split(v[[vname]], v[["frame"]], drop = TRUE))
 	neg <- lapply(spl.2, function(v) lapply(v, function(x) which(x == FALSE)))
 	pos <- lapply(spl.2, function(v) lapply(v, function(x) which(x == TRUE)))
 
