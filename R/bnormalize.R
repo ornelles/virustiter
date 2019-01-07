08dad4739a41715996c42f8ab0e901f989540573
David Ornelles <ornelles@wakehealth.edu>
2019-01-07 12:05:13 -0500

    use 5% quantile to normalize

--- R/bnormalize.R
+++ R/bnormalize.R
@@ -4,22 +4,26 @@
 #' 
 #' @param img \code{Image} grayscale image object
 #' @param size Radius for \code{medianFilter}, integer. Default value of 2 
-#' for typical images; use 0 to skip median filter.
+#'   for typical images; use 0 to skip median filter.
 #' @param sigma Standard deviation for \code{gblur}. Default value of 2 
-#' for typical images, use 5 for finely detailed images; use 0 to skip 
-#' Gaussian blur.
+#'   for typical images, use 5 for finely detailed images; use 0 to skip 
+#'   Gaussian blur.
+#' @param quant Quantile to evaluate as baseline for background signal
+#'  (default value of 0.05, use 0.5 for median value).
 #' @param offset Value (default of 0.05) added to image after subtracting 
-#' median value.
+#'  baseline value. 
 #' 
 #' @details
 #' 
 #' This is meant to be applied to images with a greater number of pixels 
-#' representing background values than foreground values. The median value 
-#' of the image will be treated as the background and each image adjusted 
-#' to have an identical median background value. The image(s) will first 
-#' be smoothed by \code{medianFilter()} with radius \code{size} then blurred 
-#' by \code{gblur} with \code{sigma}. Each processed image will be 
-#' adjusted by subtracting the median value and adding \code{offset}.
+#' representing background values than foreground values. The baseline
+#' for each frame will be determined as \code{quantile(x, quant)} where
+#' \code{x} is the pixel intensity. Each image will be adjusted 
+#' to have the identical background value at this quantile. The image(s) will
+#' first be smoothed by \code{medianFilter()} with radius \code{size} then
+#' blurred by \code{gblur} with \code{sigma}. Each processed image will be 
+#' adjusted by subtracting the background value and adding \code{offset}. Values
+#' less than 0 will be changed to 0.
 #' 
 #' @return
 #' 
@@ -42,9 +46,9 @@
 #' 
 #' @export
 #' 
-bnormalize <- function(img, size = 2, sigma = 2, offset = 0.05)
+bnormalize <- function(img, size = 2, sigma = 2, quant = 0.05, offset = 0.05)
 {
-	if (colorMode(img) != 0)
+	if (!is(img, "Image") || colorMode(img) != 0)
 		stop("grayscale image required")
 	if (size > 0) {
 		size <- as.integer(size)
@@ -52,14 +56,16 @@ bnormalize <- function(img, size = 2, sigma = 2, offset = 0.05)
 	}
 	if (sigma > 0)
 		img <- gblur(img, sigma)
+	if (quant[1] > 1 | quant[1] < 0)
+		stop("'quant' must be between 0 and 1")
 
 	dm <- dim(img)
 	if (length(dm) == 2) {
-		bgnd <- median(img)
+		bgnd <- quantile(img, quant)
 		img <- img - bgnd + offset
 	}
 	else {
-		bgnd <- apply(img, 3, median)
+		bgnd <- apply(img, 3, quantile, quant)
 		bgndImg <- Image(rep(bgnd, each = prod(dm[1:2])), dim = dm)
 		img <- img - bgndImg + offset
 	}
