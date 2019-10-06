c752e2c1e359cd9be6f982da38dc0d2571917930
David Ornelles <ornelles@wakehealth.edu>
2019-10-06 11:58:48 -0400

    allow getVal to use additional arguments

--- R/getVal.R
+++ R/getVal.R
@@ -1,21 +1,25 @@
 #' Get Value from ComputeFeatures.xxx Function
 #' 
-#' Extract a value from one of the \code{computeFeatures} family of
-#' functions using a mask or a list of masks and target image
-#' or list of images.  
+#' Extract a value through one of the \code{computeFeatures} family of
+#' functions using an object mask and reference image or a list of each.
 #'
 #' @param mask An object mask or list of objects masks with
 #'   connected pixels having the same integer value. 
 #' @param ref A fluorescent \code{Image} object or list of
-#'   \code{Image}s corresponding to the objects in \code{mask}
-#'   or \code{NULL} for \code{computeFeatures.shape}.
+#'   \code{Image} objects corresponding to the objects in \code{mask}
+#'   or \code{NULL} if a reference image is not required.
 #' @param val A character string identifying the parameter
-#'   to return from the computeFeatures function \code{FUN}. The
-#'   default value of \code{"b.mean"} returns the mean intensity.
+#'   to return from the \code{computeFeatures} function assigned
+#'   to \code{FUN}. The default value of \code{"b.mean"} returns the
+#'   mean intensity with the
+#'   \code{\link[EBImage:computeFeatures]{computeFeatures.basic}}
+#'   function.
 #' @param FUN A \code{\link[EBImage]{computeFeatures}} function to be
 #'   applied over \code{mask} and \code{ref}. The default of 
-#'   \code{NULL} uses the character string in \code{val} to identify
-#'   the appropriate function.
+#'   \code{NULL} uses the character string in \code{val} to select
+#'   the appropriate function. If this function is specified,
+#'   the value in the argument \code{val} must be an exact match
+#'   to one of the values returned by the specified function.
 #' @param simplify If \code{TRUE} (default), the result will be
 #'   collapsed into a vector. Otherwise, a vector for each member of 
 #'   the list \code{mask} and \code{ref} will be returned.
@@ -28,21 +32,38 @@
 #' value specified by \code{val} will be returned as a list if \code{
 #' simplify == FALSE} or as a single vector if \code{simplify == TRUE}.
 #'
-#' Common usages include getting the mean intensity from a \code{mask}
-#' and reference image in \code{ref} by \code{getVal(mask, ref)} or
-#' getting the area of objects from an object mask in \code{mask} by
-#' \code{getVal(mask, val = "area")}.
+#' Common usages include getting the mean intensity from an object mask
+#' and reference image  or getting the area of objects from an object mask
+#' as shown below.
+#' \preformatted{
+#' # To extract the mean intensity:
+#'   mfi <- getVal(mask, ref) # uses default parameter "b.mean"
+#'
+#' # To extract object area, collapsed to a single vector:
+#'   area <- getVal(mask, val = "area") # no need 'ref'
+#'
+#' # A complex example of Haralick difference entropy at
+#' # at a Haralick scale of 4 pixels
+#'   den <- getVal(mask, ref, val = "h.den.s4",
+#'     FUN = computeFeatures.haralick, haralick.scales = 4)
+#' }
 #' 
-#' Functions that can be explicitly specified include 
+#' The character string in \code{val} is used as a \code{grep} pattern to
+#' search among the parameters returned by the \code{computeFeatures}
+#' functions. Consequently, a partial match such as "ecc" is sufficient
+#' for a parameter such as "m.eccentricity". However, because "mean" occurs
+#' in "b.mean" and "s.radius.mean", a partial match to "mean" does not
+#' identify a unique parameter. The unique parameter is used to assign the
+#' value to \code{FUN}. This value can be explicitly assigned as one of
 #' \code{\link[EBImage:computeFeatures]{computeFeatures.basic}}, 
 #' \code{\link[EBImage:computeFeatures]{computeFeatures.shape}}, 
 #' \code{\link[EBImage:computeFeatures]{computeFeatures.moment}} and 
 #' \code{\link[EBImage:computeFeatures]{computeFeatures.haralick}}.
 #' See the \code{EBImage} help page for appropriate values for
 #' \code{FUN} and \code{val}. Note that "\code{basic quantiles}" 
-#' parameters are misnamed with an extra '0'. For example, the 5th
+#' parameters are mislabeled with an extra '0'. For example, the 5th
 #' percentile and 95th percentile values are named "\code{b.q005}"
-#' and "\code{b.q095}".
+#' and "\code{b.q095}", respectively.
 #'
 #' @return
 #' 
@@ -69,6 +90,9 @@ getVal <- function(mask, ref = NULL, val = "b.mean", FUN = NULL, simplify = TRUE
 		print(varList)
 		return(invisible(NULL))
 	}
+# capture additional arguments in ...
+	dots <- list(...)
+	if (length(dots) == 0) dots <- list(NULL)
 
 # assign computeFeatures function and variable
 	if (is.null(FUN)) { # identify function from 'val'
@@ -91,22 +115,27 @@ getVal <- function(mask, ref = NULL, val = "b.mean", FUN = NULL, simplify = TRUE
 			ref <- NULL
 	}
 # working function
-	.proc <- function(mask, ref, val, FUN, ...) {
-		if (is.null(ref))
-			unlist(lapply(getFrames(mask), function(m) FUN(m, ...)[,val]))
-		else
-			unlist(Map(function(m, t, ...) FUN(m, t, ...)[,val], 
-				getFrames(mask), getFrames(ref), ...))
+	.proc <- function(mask, ref, val, FUN, dots) {
+		if (is.null(ref)) {
+			res <- lapply(getFrames(mask), function(m)
+				do.call(FUN, args = c(list(m), dots))[,val])
+			return(unlist(res))
+		}
+		else {
+			res <- Map(FUN, getFrames(mask), getFrames(ref), MoreArgs = dots)
+			res <- do.call(rbind, res)
+			return(res[,val])
+		}
 	}
 # determine if 'mask' and 'ref' are lists or Images
 	if (is(mask, "Image") && (is(ref, "NULL") | is(ref, "Image")))
-		ans <- .proc(mask, ref, val, FUN, ...)
+		ans <- .proc(mask, ref, val, FUN, dots)
 	else if (is(mask, "list")) {
 		if (!all(sapply(mask, is, "Image")))
 			stop("'mask' must be an Image object or list of Image objects")
 		if (length(mask) != length(ref))
 			stop("'mask' and 'ref' must of the same length")
-		ans <- Map(.proc, mask, ref, val, list(FUN)) # list() needed 
+		ans <- Map(.proc, mask, ref, val, list(FUN), list(dots)) # list() needed 
 	}
 	else 
 			stop("unable to handle combination of 'mask' and 'ref'")
